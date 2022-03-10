db9c05840936d3351d3344a19089ed2a53c6807a
Joachim Hauso Amundsen <joachim.amundsen@gmail.com>
2022-03-10 16:21:45 +0100

    Search bar text field with send/search button. No functionality

--- frontend/src/header.rs
+++ frontend/src/header.rs
@@ -1,3 +1,4 @@
+
 use crate::{app, router::Route};
 use zoon::{named_color::*, *};
 
@@ -8,18 +9,22 @@ use zoon::{named_color::*, *};
 pub fn header() -> impl Element {
     Row::new()
         .s(Background::new().color(GRAY_4))
-        .s(Font::new().size(80).color(hsluv!(18,100,48,100)))
-        .s(Spacing::new(20))
 
+        .s(Font::new().size(100).color(hsluv!(18,100,48,100)))
+        .s(Spacing::new(30))
+        .s(Padding::new())
         .item(back_button())
         .item(link("Home", Route::Root))
-
+        .item(search_bar())
+        .item(send_button())
 }
+
+
 
 fn back_button() -> impl Element {
     let (hovered, hovered_signal) = Mutable::new_and_signal(false);
     Button::new()
-        .s(Background::new().color_signal(hovered_signal.map_bool(|| GREEN_7, || GREEN_8)))
+        .s(Background::new().color_signal(hovered_signal.map_bool(|| GRAY_8, || GRAY_4)))
         .s(Padding::new().x(7).y(4))
         .on_hovered_change(move |is_hovered| hovered.set(is_hovered))
         .label("< Back")
@@ -33,5 +38,27 @@ fn link(label: &str, route: Route) -> impl Element {
         .to(route)
 }
 
+fn search_bar() -> impl Element {
+    TextInput::new()
+        .s(Padding::all(20))
+        .s(RoundedCorners::new().left(5))
+        .s(Width::fill())
+        .s(Font::new().size(17))
+        .focus(true)
+       // .on_change(super::set_new_message_text)
+        .label_hidden("New message text")
+        .placeholder(Placeholder::new("Search here you clown"))
 
+}
 
+fn send_button() -> impl Element {
+    let (hovered, hovered_signal) = Mutable::new_and_signal(false);
+    Button::new()
+        .s(Padding::all(20))
+        .s(RoundedCorners::new().right(5))
+        .s(Background::new().color_signal(hovered_signal.map_bool(|| GREEN_7, || GREEN_8)))
+        .s(Font::new().color(GRAY_0).size(17))
+        .on_hovered_change(move |is_hovered| hovered.set(is_hovered))
+        //.on_press(super::send_message)
+        .label("Send")
+}
