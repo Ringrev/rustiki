9bbcea014306a87757ee11cb83f575a7e9e6e514
lindahageselle <72336824+lindahageselle@users.noreply.github.com>
2022-03-25 15:55:36 +0100

    Worked a bit on structure and added some buttons

--- frontend/src/new_article_page.rs
+++ frontend/src/new_article_page.rs
@@ -1,6 +1,6 @@
 use zoon::*;
 use zoon::events::Input;
-use zoon::named_color::{GRAY_0, GRAY_2, GRAY_4};
+use zoon::named_color::*;
 use zoon::Tag::Header;
 use zoon::text_input::InputTypeText;
 use zoon::web_sys::HtmlTextAreaElement;
@@ -9,10 +9,18 @@ use zoon::web_sys::HtmlTextAreaElement;
 pub fn page() -> impl Element {
     Column::new()
         .s(Align::center())
-        .item(Paragraph::new().content("Create new article"))
-        .item(title_panel())
-        .item(Text::with_signal(title_text().signal_cloned()))
-        .item(main_text_panel())
+        .s(Width::new(800))
+        .s(Background::new().color(hsluv!(0,0,0,5)))
+        .item(Column::new()
+            .s(Align::left(Default::default()))
+            .s(Align::center())
+            .s(Padding::new().x(100).y(20))
+            .item(Paragraph::new().content("Create new article"))
+            .item(title_panel())
+            .item(Text::with_signal(title_text().signal_cloned()))
+            .item(main_text_panel())
+        )
+        .item(button_panel())
 }
 
 // ------ state of title
@@ -106,7 +114,8 @@ fn set_main_text(main: String) {
 
 fn main_text_input(id: &str) -> impl Element {
     TextArea::new()
-        .s(Width::new(300))
+        .s(Width::new(600))
+        .s(Height::new(400))
         .s(Padding::all(10))
         .s(Shadows::new(vec![Shadow::new()
             .inner()
@@ -120,3 +129,35 @@ fn main_text_input(id: &str) -> impl Element {
 }
 
 // ------
+
+fn button_panel() -> impl Element {
+    Row::new()
+        .item(cancel_button())
+        .item(publish_button())
+        .s(Spacing::new(10))
+        .s(Align::right(Default::default()))
+}
+
+fn publish_button() -> impl Element {
+    let (hovered, hovered_signal) = Mutable::new_and_signal(false);
+    Button::new()
+        .s(Font::new().size(16).color(GRAY_0))
+        .s(Background::new()
+            .color_signal(hovered_signal.map_bool(|| GRAY_5, || GRAY_9)))
+        .s(Padding::new().y(10).x(15))
+        .on_hovered_change(move |is_hovered| hovered.set(is_hovered))
+        .label("Publish")
+        // .on_press()
+}
+
+fn cancel_button() -> impl Element {
+    let (hovered, hovered_signal) = Mutable::new_and_signal(false);
+    Button::new()
+        .s(Font::new().size(16).color(GRAY_0))
+        .s(Background::new()
+            .color_signal(hovered_signal.map_bool(|| GRAY_5, || GRAY_9)))
+        .s(Padding::new().y(10).x(15))
+        .on_hovered_change(move |is_hovered| hovered.set(is_hovered))
+        .label("Cancel")
+        // .on_press()
+}
