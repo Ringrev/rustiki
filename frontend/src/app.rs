129515b09863d2e1de4f20a1f06901db729adcfd
lindahageselle <72336824+lindahageselle@users.noreply.github.com>
2022-04-12 08:30:48 +0200

    doc stuff

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -7,44 +7,58 @@ use crate::footer::footer;
 use crate::connection::connection;
 use crate::header::{header};
 
-pub static USER_TOKEN: &str = "token";
+// Can be used for local storage of token later
+// pub static USER_TOKEN: &str = "token";
 
-// ------ page names ------
+////////////////////////////////////
+// ------ logged in/out state ------
+////////////////////////////////////
 
+/// Used to decide if user sees logged in or out view of site
 pub fn is_user_logged_signal() -> impl Signal<Item = bool> {
-    logged_user().signal_ref(Option::is_some)
+    logged_in_user().signal_ref(Option::is_some)
 }
 
+/// Boolean value which is set to true when logged_user
 pub fn is_user_logged() -> bool {
-    logged_user().map(Option::is_some)
+    logged_in_user().map(Option::is_some)
 }
 
+/// State of logged in user
 #[static_ref]
-pub fn logged_user() -> &'static Mutable<Option<User>> {
+pub fn logged_in_user() -> &'static Mutable<Option<User>> {
     Mutable::new(None)
 }
 
+/// The username of the logged in user
 #[static_ref]
 pub fn logged_user_name() -> &'static Mutable<String> {
     Mutable::new("".to_string())
 }
 
+/// Removes token and user info so the user is logged out of the site
 pub fn log_out() {
     auth_token().set(None);
-    logged_user().set(None);
+    logged_in_user().set(None);
     logged_user_name().set("".to_string());
 }
 
+/// Holds the auth token when a user is logged in
 #[static_ref]
 pub fn auth_token() -> &'static Mutable<Option<String>> {
     Mutable::new(None)
 }
 
+/// Sets user info and token when user logs in. Called from connection.rs
 pub fn set_logged_in_user_and_token(user: User) {
-    logged_user().set(Some(user.clone()));
+    logged_in_user().set(Some(user.clone()));
     logged_user_name().set(Some(user.clone()).unwrap().username);
     auth_token().set(Some(user.clone().auth_token));
 }
+
+/////////////////////////////
+// ------ page names ------
+/////////////////////////////
 
 #[derive(Clone, Copy, PartialEq, PartialOrd)]
 pub enum PageName {
@@ -81,7 +95,9 @@ fn placeholder_text() -> impl Element {
         .s(Align::new().center_y())
 }
 
+/////////////////////////////
 // ------ page routing ------
+/////////////////////////////
 
 fn page() -> impl Element {
     El::new().child_signal(page_name().signal().map(|page_name| match page_name {
